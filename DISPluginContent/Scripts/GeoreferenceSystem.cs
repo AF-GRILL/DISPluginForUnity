1b994263a5474e82e8a19ab079715e716caa438e
Elijah Crawford <ecc22803@yahoo.com>
2022-09-13 14:57:18 -0400

    Got the GeoreferenceSystem Working?

--- DISPluginContent/Scripts/GeoreferenceSystem.cs
+++ DISPluginContent/Scripts/GeoreferenceSystem.cs
@@ -26,104 +26,185 @@ public class GeoreferenceSystem : MonoBehaviour
     public double OriginAlt = 0;
 
     private Vector3Double Origin;
-    private dvec3 North, East, Down;
+    private dvec4 Offset;
 
-    private mat4 WorldFrameToECEFFrame; // Matrix to transform a vector from EU to ECEF CRS
-    private mat4 ECEFFrameToWorldFrame; // Matrix to transform a vector from ECEF to UE CRS - Inverse of the previous one kept in cache. 
+    private dmat4 WorldFrameToECEFFrame; // Matrix to transform a vector from EU to ECEF CRS
+    private dmat4 ECEFFrameToWorldFrame; // Matrix to transform a vector from ECEF to UE CRS - Inverse of the previous one kept in cache. 
 
-    private mat4 WorldFrameToUnityFrame;
-    private mat4 UnityFrameToWorldFrame;
+    private dmat4 WorldFrameToUnityFrame;
+    private dmat4 UnityFrameToWorldFrame;
 
     // Start is called before the first frame update
     void Awake()
     {
-        Vector3Double b = new Vector3Double { 
-        X = OriginLat,
-        Y = OriginLon,
-        Z = OriginAlt
+        SetupVars();
+
+        #region Debugging
+        
+        Debug.Log("Origin (ECEF): " + Origin.X + ", " + Origin.Y + ", " + Origin.Z);
+        Debug.Log("Offset (Unity): " + Offset.xyz);
+
+        Vector3Double output;
+        Conversions.CalculateEcefXYZFromLatLonHeight(new Vector3Double { X = OriginLat, Y = OriginLon, Z = OriginAlt + 1 }, out output);
+        Debug.Log("Up:   " + (GetUnityCoordinatesFromECEF(output) - Offset).xyz);
+
+        Conversions.CalculateEcefXYZFromLatLonHeight(new Vector3Double { X = OriginLat + 1, Y = OriginLon, Z = OriginAlt }, out output);
+        Debug.Log("Lat:   " + (GetUnityCoordinatesFromECEF(output) - Offset).xyz);
+
+        Conversions.CalculateEcefXYZFromLatLonHeight(new Vector3Double { X = OriginLat, Y = OriginLon + 1, Z = OriginAlt }, out output);
+        Debug.Log("Lon:   " + (GetUnityCoordinatesFromECEF(output) - Offset).xyz);
+        
+        #endregion Debugging
+    }
+
+    #region PublicFunctions
+
+    public dvec3 LatLonAltToUnity(Vector3Double LatLonAlt)
+    {
+        Vector3Double ECEF;
+        Conversions.CalculateEcefXYZFromLatLonHeight(LatLonAlt, out ECEF);
+        return (GetUnityCoordinatesFromECEF(ECEF) - Offset).xyz;
+    }
+
+    public dvec3 ECEFToUnity(Vector3Double ECEF)
+    {
+        return (GetUnityCoordinatesFromECEF(ECEF) - Offset).xyz;
+    }
+
+    public Vector3Double UnityToLatLonAlt(dvec3 Unity)
+    {
+        return GetLatLonAltFromUnity(Unity);
+
+    }
+
+    public Vector3Double UnityToECEF(dvec3 Unity)
+    {
+        return GetECEFFromUnity(Unity);
+    }
+    public Vector3Double GetOrigin() { return Origin; }
+
+    #endregion PublicFunctions
+
+    #region PrivateFunctions
+    private void SetupVars()
+    {
+        Vector3Double b = new Vector3Double
+        {
+            X = OriginLat,
+            Y = OriginLon,
+            Z = OriginAlt
         };
 
         Conversions.CalculateEcefXYZFromLatLonHeight(b, out Origin);
-        Conversions.CalculateNorthEastDownVectorsFromLatLon(OriginLat, OriginLon, out North, out East, out Down);
         GetWorldFrameToECEFFrame(Origin);
         GetWorldFrameToUnityFrame();
-        
+        Offset = GetUnityCoordinatesFromECEF(Origin);
+
     }
 
-    public Vector3Double GetOrigin() { return Origin; }
-    public dvec3 GetNorth() { return North; }
-    public dvec3 GetEast() { return East; }
-    public dvec3 GetDown() { return Down; }
-    
     private void GetWorldFrameToECEFFrame(Vector3Double ECEF)
     {
-        dvec3 Up = new dvec3(ECEF.X / Mathf.Pow((float)EARTH_MAJOR_AXIS, 2), ECEF.Y / Mathf.Pow((float)EARTH_MAJOR_AXIS, 2), ECEF.Z / Mathf.Pow((float)EARTH_MINOR_AXIS, 2));
+        if (Abs(ECEF.X) < 0.000000001 && Abs(ECEF.Y) < 0.000000001)
+        {
+            Debug.Log("Along Pole");
+            //Origin at Center
+            if (Abs(ECEF.Z) < Mathf.Epsilon)
+            {
+                //Works
+                WorldFrameToECEFFrame =
+                    new dmat4(0, -1, 0, ECEF.X,
+                              1, 0, 0, ECEF.Y,
+                              0, 0, 1, ECEF.Z,
+                              0, 0, 0, 1);
 
-        dvec3 East = new dvec3( -ECEF.Y, ECEF.X, 0).Normalized;
+                //Origin at N/S Pole
+            } else
+            {
+                //Works
+                double Sign = Mathf.Sign((float)ECEF.Z);
+                WorldFrameToECEFFrame =
+                    new dmat4(0, -1 * Sign, 0, ECEF.X,
+                              1, 0, 0, ECEF.Y,
+                              0, 0, 1 * Sign, ECEF.Z,
+                              0, 0, 0, 1);
+            }
+        }
+        else
+        {
+            dvec3 Up = new dvec3(ECEF.X / (EARTH_MAJOR_AXIS * EARTH_MAJOR_AXIS), ECEF.Y / (EARTH_MAJOR_AXIS * EARTH_MAJOR_AXIS), ECEF.Z / (EARTH_MINOR_AXIS * EARTH_MINOR_AXIS)).Normalized;
 
-        dvec3 North = dvec3.Cross(Up, East);
+            dvec3 East = new dvec3(-ECEF.Y, ECEF.X, 0).Normalized;
 
-        mat4 mat = new mat4
-            ((float) East.x,   (float) East.y,    (float) East.z,  0,
-             (float) North.x,  (float) North.y,   (float) North.z, 0,
-             (float) Up.x,     (float) Up.y,      (float) Up.z,    0,
-             (float) ECEF.X,   (float) ECEF.Y,    (float) ECEF.Z,   1);
+            dvec3 North = dvec3.Cross(Up, East);
 
 
-        WorldFrameToECEFFrame = mat;
+            dmat4 mat = new dmat4
+                (East.x, East.y, East.z, 0,
+                 North.x, North.y, North.z, 0,
+                    Up.x, Up.y, Up.z, 0,
+                  ECEF.X, ECEF.Y, ECEF.Z, 1);
+
+
+            dmat4 matTest = new dmat4
+                (East.x, North.x, Up.x, ECEF.X,
+                 East.y, North.y, Up.y, ECEF.Y,
+                 East.z, North.z, Up.z, ECEF.Z,
+                      0, 0, 0, 1);
+
+
+            WorldFrameToECEFFrame = matTest;
+        }
         ECEFFrameToWorldFrame = WorldFrameToECEFFrame.Inverse;
     }
 
     private void GetWorldFrameToUnityFrame()
     {
-        mat4 WorldFrameToUEFrame =
-            new mat4(1,  0,  0,  0,
-                     0,  -1, 0,  0,
-                     0,  0,  1,  0,
-                     0,  0,  0,  1);
+        dmat4 WorldFrameToUEFrame =
+            new dmat4(1, 0, 0, 0,
+                      0, 1, 0, 0,
+                      0, 0, 1, 0,
+                      0, 0, 0, 1);
 
-        WorldFrameToUnityFrame = WorldFrameToUEFrame * 
-            new mat4(0,  0,  1,  0,
-                     1,  0,  0,  0,
-                     0,  1,  0,  0,
-                     0,  0,  0,  1);
+        WorldFrameToUnityFrame = WorldFrameToUEFrame *
+            new dmat4(0, 0, 1, 0,
+                      1, 0, 0, 0,
+                      0, 1, 0, 0,
+                      0, 0, 0, 1);
 
         UnityFrameToWorldFrame = WorldFrameToUnityFrame.Inverse;
     }
 
-    public vec4 GetUnityCoordinatesFromECEF(Vector3Double ECEF)
+    private dvec4 GetUnityCoordinatesFromECEF(Vector3Double ECEF)
     {
-        vec4 multiplier = new vec4((float)ECEF.X, (float)ECEF.Y, (float)ECEF.Z, 1);
+        dvec4 multiplier = new dvec4((float)ECEF.X, (float)ECEF.Y, (float)ECEF.Z, 1);
 
-        float elem0 = vec4.Dot(ECEFFrameToWorldFrame.Row0, multiplier);
-        float elem1 = vec4.Dot(ECEFFrameToWorldFrame.Row1, multiplier);
-        float elem2 = vec4.Dot(ECEFFrameToWorldFrame.Row2, multiplier);
-        float elem3 = vec4.Dot(ECEFFrameToWorldFrame.Row3, multiplier);
+        dvec4 WorldCoords = ECEFFrameToWorldFrame * multiplier;
+        dvec4 UnityCoords = WorldFrameToUnityFrame * WorldCoords;
 
-        return new vec4(elem0, elem1, elem2, elem3);
+        return UnityCoords;
     }
 
+    private Vector3Double GetECEFFromUnity(dvec3 Unity)
+    {
+        dvec4 Unity4 = new dvec4(Unity, 0);
+        Unity4 += Offset;
+        dvec4 WorldFrame = UnityFrameToWorldFrame * Unity4;
+        dvec4 ECEF = WorldFrameToECEFFrame * WorldFrame;
+        return new Vector3Double { X = ECEF.x, Y = ECEF.y, Z = ECEF.z };
+    }
 
-    #region OldMath
-    ///OldMath
-    /*
-    dvec2 N2 = new dvec2(North.x, North.z);
-    heading = (Mathf.Acos((float)(dvec2.Dot(N2, -dvec2.UnitY) / (N2.Length * Mathf.Sqrt(1.0f))))) * 180 / Mathf.PI;
-    Debug.Log("Heading: " + heading + "  Math: " + (180 - OriginLat));
+    private Vector3Double GetLatLonAltFromUnity(dvec3 Unity)
+    {
+        Vector3Double ECEF = GetECEFFromUnity(Unity);
+        Vector3Double LatLonHeight;
+        Conversions.CalculateLatLonHeightFromEcefXYZ(ECEF, out LatLonHeight);
+        return LatLonHeight;
+    }
 
-    dvec2 E2 = new dvec2(East.z, East.y);
-    pitch = (Mathf.Acos((float)(dvec2.Dot(E2, dvec2.UnitX) / (E2.Length * Mathf.Sqrt(1.0f))))) * 180 / Mathf.PI;
-    Debug.Log("Pitch: " + pitch);
-
-    dvec2 D2 = new dvec2(Down.x, Down.y);
-    roll = (Mathf.Acos((float)(dvec2.Dot(D2, -dvec2.UnitY) / (D2.Length * Mathf.Sqrt(1.0f))))) * 180 / Mathf.PI;
-    Debug.Log("Roll: " + roll + "  Math: " + (90 - OriginLon));
-
-    Conversions.ApplyHeadingPitchRollToNorthEastDownVector(heading, pitch, roll, North, East, Down, out testN, out testE, out testD);
-    Debug.Log(testN);
-    Debug.Log(testE);
-    Debug.Log(testD);
-    */
-    ///
-    #endregion OldMath
+    private double Abs(double i)
+    {
+        if (i < 0) { return -i; }
+        else { return i; }
+    }
+    #endregion PrivateFunctions
 }
