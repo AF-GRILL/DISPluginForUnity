7f377763ff9931469109b108c6020ac7852b5a5b
huggins <huggy2014@yahoo.com>
2023-01-13 14:18:23 -0500

    Moved sample content into a Samples folder.

---
+++ DISPluginContent/Samples/Sample_HandleDISUpdates.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenDis.Dis1998;
+using GRILLDIS;
+
+public class Sample_HandleDISUpdates : MonoBehaviour
+{
+    private GeoreferenceSystem georeferenceSystemScript;
+    private void Awake()
+    {
+        georeferenceSystemScript = FindObjectOfType<GeoreferenceSystem>();
+    }
+
+    public void Start()
+    {
+    }
+
+    public void HandleDeadReckoningUpdate(EntityStatePdu DeadReckonedPDUIn)
+    {
+        Conversions.GetUnityLocationAndOrientationFromEntityStatePdu(DeadReckonedPDUIn, georeferenceSystemScript, out Vector3Double unityLoc, out Vector3 unityRot);
+        transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
+        transform.rotation = Quaternion.Euler(unityRot);
+    }
+
+    public void HandleEntityStateProcessed(EntityStatePdu EntityStatePDUIn)
+    {
+        Conversions.GetUnityLocationAndOrientationFromEntityStatePdu(EntityStatePDUIn, georeferenceSystemScript, out Vector3Double unityLoc, out Vector3 unityRot);
+        transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
+        transform.rotation = Quaternion.Euler(unityRot);
+    }
+
+}
