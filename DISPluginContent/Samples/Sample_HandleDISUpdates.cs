6c8857dad913c494cc96eabff79c5da590fd220c
Enrico Speranza <enrico.speranza@gmail.com>
2023-02-27 14:44:33 +0100

    Update Sample_HandleDISUpdates.cs
    Using single instruction transform.SetPositionAndRotation

--- DISPluginContent/Samples/Sample_HandleDISUpdates.cs
+++ DISPluginContent/Samples/Sample_HandleDISUpdates.cs
@@ -21,15 +21,17 @@ namespace GRILLDIS.Sample
         public void HandleDeadReckoningUpdate(EntityStatePdu DeadReckonedPDUIn)
         {
             Conversions.GetUnityLocationAndOrientationFromEntityStatePdu(DeadReckonedPDUIn, georeferenceSystemScript, out Vector3Double unityLoc, out Vector3 unityRot);
-            transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
-            transform.rotation = Quaternion.Euler(unityRot);
+            transform.SetPositionAndRotation(new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z), Quaternion.Euler(unityRot));
+            //transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
+            //transform.rotation = Quaternion.Euler(unityRot);
         }
 
         public void HandleEntityStateProcessed(EntityStatePdu EntityStatePDUIn)
         {
             Conversions.GetUnityLocationAndOrientationFromEntityStatePdu(EntityStatePDUIn, georeferenceSystemScript, out Vector3Double unityLoc, out Vector3 unityRot);
-            transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
-            transform.rotation = Quaternion.Euler(unityRot);
+            transform.SetPositionAndRotation(new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z), Quaternion.Euler(unityRot));
+            //transform.position = new Vector3((float)unityLoc.X, (float)unityLoc.Y, (float)unityLoc.Z);
+            //transform.rotation = Quaternion.Euler(unityRot);
         }
 
     }
